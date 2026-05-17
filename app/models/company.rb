cb1494f0b0d46955d63cee0f0f78ea1f68886130
Rafael Ramirez <raframzs@gmail.com>
2026-05-16 20:36:05 -0500

    feat: add callback to destroy orphaned non-admin users upon company deletion

--- app/models/company.rb
+++ app/models/company.rb
@@ -12,11 +12,19 @@ class Company < ApplicationRecord
 
   DEFAULT_WHATSAPP_TEMPLATE = "Hola {{cliente}}, te compartimos tu pedido {{factura}}:\n\n{{items}}\n\nTotal: {{total}}\n\n¡Gracias por tu compra!"
 
+  after_destroy :cleanup_orphaned_users
+
   def next_invoice_number!
     with_lock do
       seq = invoice_seq + 1
       update_columns(invoice_seq: seq)
       "F-#{seq.to_s.rjust(5, '0')}"
     end
+  end
+
+  private
+
+  def cleanup_orphaned_users
+    User.where(super_admin: false).where.missing(:companies).destroy_all
   end
 end
